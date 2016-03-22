f4455333809ec818430e9507651b1b90d8897889
Johannes C. Schulz <info@enzephalon.de>
2016-03-22 10:05:26 +0100

    the dots were right, more debug

--- Classes/Validator/ErrorCheck/Cleverreachemail.php
+++ Classes/Validator/ErrorCheck/Cleverreachemail.php
@@ -41,9 +41,9 @@ class Cleverreachemail extends \Typoheads\Formhandler\Validator\ErrorCheck\Abstr
     public function check() {
         $checkFailed = '';
 
-        $soap = new \SoapClient($this->settings['params']['config']['wsdlUrl']);
-
-        $return = $soap->receiverGetByEmail($this->settings['params']['config']['apiKey'], $this->settings['params']['config']['listId'], trim($this->gp[$this->formFieldName]), 0);
+        $soap = new \SoapClient($this->settings['params']['config.']['wsdlUrl']);
+var_dump($soap);
+        $return = $soap->receiverGetByEmail($this->settings['params']['config.']['apiKey'], $this->settings['params']['config.']['listId'], trim($this->gp[$this->formFieldName]), 0);
         if ($return->statuscode == 1)
             return "apikey";
 var_dump($return);
